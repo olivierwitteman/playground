213f11271ed003868df4f65150da102a2ca774df
Olivier Witteman <olivier@2001.net>
2020-11-18 16:56:01 +0100

    use stdout instead of writing from script

--- MechanicalSoup/vanmoof_tracker.py
+++ MechanicalSoup/vanmoof_tracker.py
@@ -15,4 +15,4 @@ browser.submit_selected()
 #     log.write(f'{int(time.time())},{browser.get_current_page().find_all("div", class_="m-info__
 #     body-details m-info__side-by-side-details--item order-line")[-1].text.strip()}\n')
 
-print(f'{int(time.time())},{browser.get_current_page().find_all("div", class_="m-info__body-details m-info__side-by-side-details--item order-line")[-1].text.strip()}\n')
+print(f'{int(time.time())},{browser.get_current_page().find_all("div", class_="m-info__body-details m-info__side-by-side-details--item order-line")[-1].text.strip()}')
