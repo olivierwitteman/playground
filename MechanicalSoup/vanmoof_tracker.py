fbcdd16d4ec6bbf6eea7d88088fa4521ccc18127
Olivier Witteman <olivier@2001.net>
2020-11-23 17:04:41 +0100

    use stdout instead of writing from script

--- MechanicalSoup/vanmoof_tracker.py
+++ MechanicalSoup/vanmoof_tracker.py
@@ -1,6 +1,7 @@
 import mechanicalsoup
 from secrets import vm_user, vm_pwd
 import time
+import sys
 
 browser = mechanicalsoup.StatefulBrowser()
 browser.open("https://www.vanmoof.com/my-vanmoof/")
@@ -15,4 +16,7 @@ browser.submit_selected()
 #     log.write(f'{int(time.time())},{browser.get_current_page().find_all("div", class_="m-info__
 #     body-details m-info__side-by-side-details--item order-line")[-1].text.strip()}\n')
 
-print(f'{int(time.time())},{browser.get_current_page().find_all("div", class_="m-info__body-details m-info__side-by-side-details--item order-line")[-1].text.strip()}')
+try:
+    print(f'{int(time.time())},{browser.get_current_page().find_all("div", class_="m-info__body-details m-info__side-by-side-details--item order-line")[-1].text.strip()}')
+except:
+    print(f'{int(time.time())},{sys.exc_info()[0]}')
